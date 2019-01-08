41a8403e8e36c41c8da5b86a7ae92caa76125046
sundowndev <raphael@crvx.fr>
2019-01-08 14:47:59 +0100

    Delete isRoot function

--- install.sh
+++ install.sh
@@ -1,17 +1,7 @@
 #!/usr/bin/env bash
 
-function isRoot () {
-        if [ "$EUID" -ne 0 ]; then
-                return 1
-        fi
-}
-
-if ! isRoot; then
-  echo "You need to run this as root!"
-  exit 1
-fi
-
 sudo wget -O /usr/bin/covermyass https://raw.githubusercontent.com/sundowndev/covermyass/master/covermyass.sh
 sudo chmod +x /usr/bin/covermyass
 
 echo "Installation succeeded."
+echo "Type covermyass to use the tool."
