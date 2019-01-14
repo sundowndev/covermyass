7fa0fa40512aaeb686f10aaf9c567444dd3abaaa
sundowndev <raphael@crvx.fr>
2019-01-14 15:08:30 +0100

    Now option & refactor

--- covermyass.sh
+++ covermyass.sh
@@ -6,56 +6,34 @@ function isRoot () {
         fi
 }
 
-clear # Clear output
-
-echo
-echo "Welcome to Cover my ass tool !"
-
-echo
-echo "Select an option :"
-echo
-echo "1) Clear auth & bash history for user $USER"
-echo "2) Permenently disable bash history"
-echo "3) Restore settings to default"
-echo "99) Exit tool"
-echo
-
-printf "> "
-read -r option
-echo
-
-if [[ $option == 1 ]]; then
-        # Clear current history
-        if [ -w /var/log/auth.log ]; then
-                echo "" > /var/log/auth.log
-                echo "[+] /var/log/auth.log cleaned."
-        else
-                echo "[!] /var/log/auth.log is not writable! Retry using sudo."
-        fi
-
-        if [ -a ~/.zsh_history ]; then
-                echo "" > ~/.zsh_history
-                echo "[+] ~/.zsh_history cleaned."
-        fi
-
-        echo "" > ~/.bash_history
-        echo "[+] ~/.bash_history cleaned."
-
-        history -c
-        echo "[+] History file deleted."
+function menu () {
+        echo
+        echo "Welcome to Cover my ass tool !"
 
         echo
-        echo "Reminder: your need to reload the session to see effects."
-        echo "Type exit to do so."
-elif [[ $option == 2 ]]; then
+        echo "Select an option :"
+        echo
+        echo "1) Clear auth & bash history for user $USER"
+        echo "2) Permenently disable auth & bash history"
+        echo "3) Restore settings to default"
+        echo "99) Exit tool"
+        echo
+
+        printf "> "
+        read -r option
+        echo
+}
+
+function disableAuth () {
         if [ -w /var/log/auth.log ]; then
                 ln /dev/null /var/log/auth.log -sf
                 echo "[+] Permanently sending /var/log/auth.log to /dev/null"
         else
                 echo "[!] /var/log/auth.log is not writable! Retry using sudo."
         fi
-        
-        # Permenently disable bash log
+}
+
+function disableHistory () {
         ln /dev/null ~/.bash_history -sf
         echo "[+] Permanently sending bash_history to /dev/null"
 
@@ -73,14 +51,19 @@ elif [[ $option == 2 ]]; then
 
         echo
         echo "Permenently disabled bash log."
-elif [[ $option == 3 ]]; then
-        if [ -w /var/log/auth.log && -L /var/log/auth.log ]; then
+}
+
+function enableAuth () {
+        if [ -w /var/log/auth.log ] && [ -L /var/log/auth.log ]; then
                 rm -rf /var/log/auth.log
                 echo "" > /var/log/auth.log
                 echo "[+] Disabled sending auth logs to /dev/null"
+        else
+                echo "[!] /var/log/auth.log is not writable! Retry using sudo."
         fi
-        
-        # Restore default settings
+}
+
+function enableHistory () {
         if [[ -L ~/.bash_history ]]; then
                 rm -rf ~/.bash_history
                 echo "" > ~/.bash_history
@@ -102,8 +85,64 @@ elif [[ $option == 3 ]]; then
 
         echo
         echo "Permenently enabled bash log."
-elif [[ $option == 99 ]]; then
+}
+
+function clearAuth () {
+        if [ -w /var/log/auth.log ]; then
+                echo "" > /var/log/auth.log
+                echo "[+] /var/log/auth.log cleaned."
+        else
+                echo "[!] /var/log/auth.log is not writable! Retry using sudo."
+        fi
+}
+
+function clearHistory () {
+        if [ -a ~/.zsh_history ]; then
+                echo "" > ~/.zsh_history
+                echo "[+] ~/.zsh_history cleaned."
+        fi
+
+        echo "" > ~/.bash_history
+        echo "[+] ~/.bash_history cleaned."
+
+        history -c
+        echo "[+] History file deleted."
+
+        echo
+        echo "Reminder: your need to reload the session to see effects."
+        echo "Type exit to do so."
+}
+
+function exitTool () {
         exit 1
+}
+
+clear # Clear output
+
+# "now" option
+if [ -n "$1" ] && [ $1 == 'now' ]; then
+        clearAuth
+        clearHistory
+        exitTool
+fi
+
+menu
+
+if [[ $option == 1 ]]; then
+        # Clear current history
+        clearAuth
+        clearHistory
+elif [[ $option == 2 ]]; then
+        # Permenently disable auth & bash log
+        disableAuth
+        disableHistory
+elif [[ $option == 3 ]]; then
+        # Restore default settings
+        enableAuth
+        enableHistory
+elif [[ $option == 99 ]]; then
+        # Exit tool
+        exitTool
 else
         echo "[!] Option not reconized. Exiting."
 fi
